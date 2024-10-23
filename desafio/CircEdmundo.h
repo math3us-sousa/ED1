1c864663beef12879578a9ec5215c70dea5ed6a3
math3us-sousa <sousa.matheus13.sousa@gmail.com>
2024-10-23 10:46:14 -0300

    Update CircEdmundo.h

--- desafio/CircEdmundo.h
+++ desafio/CircEdmundo.h
@@ -9,7 +9,4 @@ Sorteio* list_create();                // Cria uma nova lista circular
 bool insert_begin(Sorteio*, int);       // Insere no início
 void print(Sorteio*);                   // Imprime a lista
 bool List_remove(Sorteio*, int);        // Remove um elemento da lista
-int list_size(Sorteio* lista);          //Retorna o tamanho da lista
-int get_soldado_at(Sorteio* list, int pos);  //Retorna o soldado da posição desejada
 #endif
-
