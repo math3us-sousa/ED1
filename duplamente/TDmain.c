1b871a60c48be652de95cb5180d113fdb18e70dd
math3us-sousa <sousa.matheus13.sousa@gmail.com>
2024-10-30 00:16:53 -0300

    Update TDmain.c

--- duplamente/TDmain.c
+++ duplamente/TDmain.c
@@ -1,11 +1,18 @@
 #include "TDLinkedList.h"
 #include <stdio.h>
-int main(){
-    TDLinkedList* lista1 = TDLinkedList_Create();
-    for(int i = 0; i <= 10; i++){
-        if(!TDLinkedList_insert_end(lista1, i))
-            printf("Erro ao inserir %d\n", i);
+
+int main (void){
+    TDLinkedList* lista1 = list_create();
+    TDLinkedList* lista2 = list_create();
+    for(int i = 1; i<=10; i++){
+        insert_end(lista1, i);
     }
-    TDLinkedList_print(lista1);
-    TDLinkedList_print_reverse(lista1);
+    insert_ordem(lista2, 0);
+    insert_ordem(lista2, 4);
+    insert_ordem(lista2, 3);
+    insert_ordem(lista2, 1);
+    insert_ordem(lista2, 5);
+    print(lista1);
+    printf("\n");
+    print(lista2);
 }
