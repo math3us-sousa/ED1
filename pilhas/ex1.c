87ff091edaee84db428947b890bd7928c1e5098a
Matheus de Sousa <sousa.matheus13.sousa@gmail.com>
2024-11-13 09:48:26 -0300

    Create ex1.c

---
+++ pilhas/ex1.c
@@ -0,0 +1,33 @@
+#include "Stack.h"
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  char[1024]
+  fgets(string,1024,stdin);
+  puts(string);
+  Stack* stack = create();
+
+  for(int i = 0; i<strlen(string);i++){
+    if(string[i] == '\n')
+      break;
+    if(string[i]==' ' || string[i]=='.')
+      print_stack(stack);
+    else
+  }
+
+
+  return 0;
+}
+
+
+
+/*função no .h
+void print_stack(Stack* stack){
+  char item;
+  while(pop(stack, &item))
+    printf("%c", item);
+  putchar(' ');
+  }
+
+*/
