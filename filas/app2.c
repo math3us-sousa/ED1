08a35983518be163bcabe2caf9540f35632b45b8
Matheus de Sousa <sousa.matheus13.sousa@gmail.com>
2024-11-25 17:18:38 -0300

    Add files via upload

---
+++ filas/app2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "Queue.h"
+
+int main(){
+    int V[] = {0,-1,2,3,-4,5,6,-7,8,9};
+    Queue* fila = create_fila();
+
+    for(int i = 0; i<10; i++)
+        if(!enqueue_fila(fila,V[i]))
+            printf("Não foi inserido V[%d]=%d.\n", i, V[i]);
+    
+    int temp;
+    fila = remove_negativo(fila);
+    while(dequeue_fila(fila, &temp))
+        printf("%d,", temp);
+    printf("\n");
+    return 0;
+}
