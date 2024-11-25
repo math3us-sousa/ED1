d2739af3c2f3d2329dd71c841282510e1c720ff6
Matheus de Sousa <sousa.matheus13.sousa@gmail.com>
2024-11-25 17:16:30 -0300

    Create Queue.h

---
+++ filas/ex1/Queue.h
@@ -0,0 +1,19 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+typedef struct queue Queue;
+
+Queue* create_fila();
+void destroy_fila(Queue*);
+int enqueue_fila(Queue*, int);
+int dequeue_fila(Queue*, int*);
+int head_fila(Queue*, int*);
+
+int queue_full(Queue*);
+int queue_empty(Queue*);
+int queue_qty(Queue*);
+
+//ex1
+int reverse_fila(Queue*);
+
+#endif
