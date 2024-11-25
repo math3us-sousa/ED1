25ac36fbd416058d84a37c28dab02f147c1373f6
Matheus de Sousa <sousa.matheus13.sousa@gmail.com>
2024-11-25 17:24:23 -0300

    Create app2.c

---
+++ filas/ex4/app2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "Queue.h"
+
+int main(){
+    int V[] = {0,1,2,3,4,5,6,7,8,9};
+    Queue* fila = create_fila();
+
+    for(int i = 0; i<10; i++)
+        if(!enqueue_fila(fila,V[i]))
+            printf("Não foi inserido V[%d]=%d.\n", i, V[i]);
+    
+    print(fila);
+    return 0;
+}
